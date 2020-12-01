a7ed51fe5954ac41d1d8457ba8cd40813fc0df06
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-01 10:57:01 +0530

    sytke: add a button

---
+++ app.js
@@ -0,0 +1,6 @@
+var txtInput = document.querySelector('#txt-input');
+var translateBtn = document.querySelector('#btn-translate');
+
+translateBtn.addEventListener('click',function InputTextHandler() {
+    txtInput
+})
