255e34e29a68360dab679bd4736337a555d083f1
Asmit shrivastava <asmitshrivastava8@gmail.com>
2020-12-01 12:08:04 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-var txtInput = document.querySelector('#txt-input');
 var translateBtn = document.querySelector('#btn-translate');
+var txtInput = document.querySelector('#txt-input');
+var outputDiv = document.querySelector('#output');
 
-translateBtn.addEventListener('click',function InputTextHandler() {
-    txtInput
-})
+
+var url = "https://api.funtranslations.com/translate/pig-latin.json";
+
+function serverUrl(text) {
+    return url + "?" + "text=" +text
+}
+
+function translate() {
+      var textInput = txtInput.value;
+      fetch(serverUrl(textInput))
+}
+
+translateBtn.addEventListener('click',translate);
